414a30b538cf21a7f9129b514ecd8c07503b2686
aleal04 <46423443+aleal04@users.noreply.github.com>
2022-01-22 11:36:57 -0600

    GyroImage

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+import {PixelRatio, View, Dimensions, Image, StyleSheet} from 'react-native';
+import {gyroscope} from 'react-native-sensors';
+const window = Dimensions.get('window');
+
+const deviceWidth = window.width;
+const deviceHeight = window.height;
+
+const imageWidth = 8 * deviceWidth;
+const imageHeight = deviceHeight;
+
+const App = () => {
+  const [axisY, setAxisY] = useState(0);
+  const [axisX, setAxisX] = useState(0);
+  const image = `https://placeimg.com/${PixelRatio.getPixelSizeForLayoutSize(
+    imageWidth,
+  )}/${PixelRatio.getPixelSizeForLayoutSize(imageHeight)}/any`;
+
+  useEffect(() => {
+    let subscription = gyroscope.subscribe(({ x , y }) => { setAxisY(y) , setAxisX(x) });
+
+    return () => {
+      subscription.unsubscribe();
+      subscription = null;
+    };
+  } , []);
+
+  /* Eje x */
+  const positionOnScreenX = -imageWidth / 2;
+  const movementX = (-axisY / 1000) * imageWidth;
+
+  /* Eje Y */
+  const positionOnScreenY = 0;
+  const movementY = (-axisX / 1000) * imageHeight;
+  return (
+    <View>
+      <Image
+        source={{uri: image}}
+        style={styles.image}
+        translateX={positionOnScreenX + movementX}
+        translateY={positionOnScreenY + movementY}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  image: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: imageHeight,
+    width: imageWidth,
+  },
+});
+
+export default App;
